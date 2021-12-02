75f923c1971afee6d498a54b5d55ef428d12d024
Lukas Schumacher <lu.schumacher@bluewin.ch>
2021-12-02 16:52:02 +0100

    add data

---
+++ data/motion_data_prep.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(magrittr)
+
+# df <- read_csv("/Users/lukas/Desktop/imagery_data.csv")
+df <- read_csv("/Users/lukas/Desktop/imagery_data_7005.csv")
+
+# resp = "left" --> 1
+# instruction = "left" --> 1
+df %<>% 
+  select(id, condition, instruction,
+         motion_duration, motion,
+         response, rt) %>% 
+  rename(resp = response) %>% 
+  mutate(id = rep(seq(1, length(unique(id))), each=nrow(df) / length(unique(id))),
+         resp = ifelse(resp == "left", 1, 0),
+         instruction = ifelse(instruction == "left", 1, 0),
+         rt = rt / 1000)
+
+
+write_csv(df, "/Users/lukas/documents/GitHub/evidence-accumulators/data/single_sub_data.csv")
