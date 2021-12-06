21353ab97fe23bf1fc928f377c4f7c5c1d4fd26e
Lukas Schumacher <lu.schumacher@bluewin.ch>
2021-12-06 17:33:48 +0100

    adding constant dm workflow

--- src/constant_drift_diffusion.py
+++ src/constant_drift_diffusion.py
@@ -5,7 +5,7 @@ from accumulators import *
 from motion_simulation import *
 
 @njit
-def var_dm_simulator(theta, n_obs, motion_set, s=0.1, dt=0.001, max_iter=1e4):
+def const_dm_simulator(theta, n_obs, amplitude, s=0.1, dt=0.001, max_iter=1e4):
     # parameters
     a     = theta[0] # boundary separation
     ndt   = theta[1] # non-decision time
@@ -18,8 +18,8 @@ def var_dm_simulator(theta, n_obs, motion_set, s=0.1, dt=0.001, max_iter=1e4):
 
     # iterate over trials
     for n in range(n_obs):
-        drift = kappa * motion_set[n]
-        rt[n], resp[n] = varying_evidence_accumulation(drift, a, ndt, bias, s, dt, max_iter)
+        drift = kappa * amplitude[n]
+        rt[n], resp[n] = const_evidence_accumulation(drift, a, ndt, bias, s, dt, max_iter)
 
     return rt, resp
 
@@ -34,22 +34,15 @@ def var_dm_priors(n_sim=1):
 
     return theta
 
-def var_dm_batch_simulator(n_sim, n_obs):
+def const_dm_batch_simulator(n_sim, n_obs):
     prior_samples = var_dm_priors(n_sim)
-
-    # create an motion experiment with fixed design (temporarily)
-    # maybe I could just give the acceleration peak intensity to BayesFlow instead of
-    # one-hot-encoded condition 2darray.
-    # n_unique_motions = 5
-    # motion_dur = 2
-    # motion_set, condition = motion_experiment(n_obs, n_unique_motions, motion_dur)
 
     unique_motions = np.array([-0.725, -0.675, -0.625, -0.575, -0.525, 0.525,  0.575,  0.625,  0.675,  0.725], dtype=np.float32)
     amplitude = np.repeat(unique_motions, 10)
-    motion_set, condition = motion_experiment_manual(1, amplitude, 1)
+    condition = to_categorical(pd.factorize(amplitude)[0])
 
     # simulate
-    sim_data = var_dm_batch_simulator_wrap(prior_samples, motion_set, condition, n_sim, n_obs)
+    sim_data = const_dm_batch_simulator_wrap(prior_samples, amplitude, condition, n_sim, n_obs)
 
     # data prep
     one_hot_encoded_resp = to_categorical(sim_data[:, :, 1])
@@ -57,17 +50,17 @@ def var_dm_batch_simulator(n_sim, n_obs):
 
 # wrapper function for faster simulation
 @njit
-def var_dm_batch_simulator_wrap(prior_samples, motion_set, condition, n_sim, n_obs):
+def const_dm_batch_simulator_wrap(prior_samples, amplitude, condition, n_sim, n_obs):
     sim_data = np.zeros((n_sim, n_obs, condition.shape[1] + 2), dtype=np.float32)
     # iterate over simulations
     for sim in range(n_sim):
         # shuffle motion profile together with condition
-        shuffler = np.random.permutation(len(motion_set))
-        motion_set = motion_set[shuffler, :]
+        shuffler = np.random.permutation(len(amplitude))
+        amplitude = amplitude[shuffler]
         condition = condition[shuffler, :]
 
         # simulate trials
-        rt, resp = var_dm_simulator(prior_samples[sim], n_obs, motion_set)
-        # sim_data[sim] = np.c_((rt, resp, condition))
+        rt, resp = const_dm_simulator(prior_samples[sim], n_obs, amplitude)
         sim_data[sim] = np.hstack((np.expand_dims(rt, axis=1), np.expand_dims(resp, axis=1), condition[0:n_obs, :]))
+
     return sim_data
